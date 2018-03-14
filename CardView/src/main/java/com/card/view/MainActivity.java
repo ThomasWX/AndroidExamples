fee6834b9197eb208a1397c2f479515095fbfac4
wang_bin <wang_bin@gionee.com>
2018-03-14 15:29:48 +0800

    Card View

---
+++ CardView/src/main/java/com/card/view/MainActivity.java
@@ -0,0 +1,19 @@
+package com.card.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, CardViewFragment.newInstance())
+                    .commit();
+        }
+    }
+}
